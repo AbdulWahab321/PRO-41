d233921d28bfb2f515e517a15a16cbe5cc91b01d
AbdulWahab321 <76244094+AbdulWahab321@users.noreply.github.com>
2021-04-05 10:23:56 +0530

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,45 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        this.score =0;
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+    static updatescore(rank){
+        database.ref('/').update({
+          Score:rank
+        })
+       }
+ 
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:score
+        });
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+
+    
+}
